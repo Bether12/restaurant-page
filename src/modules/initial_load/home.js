255e5a30cdebd3d7700270d25e2724a559b936f0
Bether12 <elnenogomez13@gmail.com>
2026-03-08 15:26:34 -0400

    Set home styles

--- src/modules/initial_load/home.js
+++ src/modules/initial_load/home.js
@@ -7,12 +7,16 @@ export const homeLoader = function(container){
     const subtitle = document.createElement("h2");
     subtitle.textContent = "Where you can taste the stars";
 
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'title';
+    titleDiv.appendChild(title);
+    titleDiv.appendChild(subtitle);
+
     //create an img
 
     const text = document.createElement('p');
-    text.textContent = 'Founded in 1997, Stardust Restaurant always aimed to turn cooking from just an art to a precise science, implementing both the latest and well stablish scientific discoveries in related fields to give the best possible dishes to our clients';
+    text.textContent = 'Founded in 1997, Stardust Restaurant always aimed to turn cooking from just an art to a precise science, implementing both the latest and well stablish scientific discoveries in related fields to give the best possible dishes to our clients.';
 
-    container.appendChild(title);
-    container.appendChild(subtitle);
+    container.appendChild(titleDiv);
     container.appendChild(text);
 };

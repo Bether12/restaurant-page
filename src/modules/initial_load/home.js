c26a214c8e0191c54a802a336498995358697e09
Bether12 <elnenogomez13@gmail.com>
2026-03-08 15:36:56 -0400

    Add logo.png to assets and erase initialBackground.png. Add logo to home.js to append when homeLoader() is called

--- src/modules/initial_load/home.js
+++ src/modules/initial_load/home.js
@@ -1,4 +1,5 @@
 import './home_styles.css';
+import logo from '../../assets/logo.png';
 
 export const homeLoader = function(container){
     const title = document.createElement("h1");
@@ -12,11 +13,13 @@ export const homeLoader = function(container){
     titleDiv.appendChild(title);
     titleDiv.appendChild(subtitle);
 
-    //create an img
+    const img = document.createElement('img');
+    img.src = logo;
 
     const text = document.createElement('p');
     text.textContent = 'Founded in 1997, Stardust Restaurant always aimed to turn cooking from just an art to a precise science, implementing both the latest and well stablish scientific discoveries in related fields to give the best possible dishes to our clients.';
 
     container.appendChild(titleDiv);
+    container.appendChild(img);
     container.appendChild(text);
 };
